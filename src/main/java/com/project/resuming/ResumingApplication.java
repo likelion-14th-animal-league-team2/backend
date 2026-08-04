a1eff832145b57618a668ceb51a6b15f78d55cd5
hururu1234 <hururu123@naver.com>
2026-08-04 13:26:18 +0900

    chore : 서비스 초기 설정 파일
    - 자바 21
    - 스프링 4.1.0

---
+++ src/main/java/com/project/resuming/ResumingApplication.java
@@ -0,0 +1,13 @@
+package com.project.resuming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/resuming/ResumingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.resuming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
