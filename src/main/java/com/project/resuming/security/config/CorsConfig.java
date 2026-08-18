8034e1c0570681c215605e6c1de3336ee11b73b4
Park, Haejeong <qocouo7@gmail.com>
2026-08-18 13:24:59 +0900

    chore: 프론트엔드 배포 서버

--- src/main/java/com/project/resuming/security/config/CorsConfig.java
+++ src/main/java/com/project/resuming/security/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of("http://localhost:8080"// + 프론트 배포주소
+        config.setAllowedOriginPatterns(List.of("http://localhost:8080","https://front-blush-tau.vercel.app/"// + 프론트 배포주소
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
